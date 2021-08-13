829b80dc8ec1bffd7f9467bbdb43fbc5b2868112
Shivam Pandey <54200130+ShivamPandey001@users.noreply.github.com>
2021-08-13 11:30:11 +0530

    Update min_step_one.cpp

--- min_step_one.cpp
+++ min_step_one.cpp
@@ -11,8 +11,6 @@ int minSteps(int n,int dp[]){
 	int op1,op2,op3;
 	op1=op2=op3=INT_MAX;
 	
-		
-
 	if(n%3==0)
 		op1=minSteps(n/3,dp)+1;
 	if(n%2==0)
@@ -23,6 +21,29 @@ int minSteps(int n,int dp[]){
 	int ans=min(min(op1,op2),op3);
 	return dp[n]=ans;
 }
+
+
+int minStepsBu(int n)
+{
+	int dp[100]={0};	
+	for(int i=2;i<=n;i++){
+		int op1,op2,op3;
+		op1=op2=op3=INT_MAX;
+		if(n%3==0)
+			op1=dp[i/3];
+		if(n%2==0)
+			op2=dp[i/2];
+		else
+			op2=dp[i-1];
+		dp[i]= min(min(op1,op2),op3)+1;
+		
+	}
+	return dp[n];
+}	
+	
+	
+	
+	
 int main(){
 	int n;
 	cin>>n;
